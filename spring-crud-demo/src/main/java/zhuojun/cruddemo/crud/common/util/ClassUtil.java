8e9e2be6f128bcf6a5dd4076b89b0fdab5a396ff
zhuojun <630302402@qq.com>
2020-06-06 11:31:30 +0800

    add util to covert a object to its child class

---
+++ spring-crud-demo/src/main/java/zhuojun/cruddemo/crud/common/util/ClassUtil.java
@@ -0,0 +1,33 @@
+package zhuojun.cruddemo.crud.common.util;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+
+/**
+ * @author: zhuojun
+ * @description:
+ * @date: 2020/06/05 22:46
+ * @modified:
+ */
+public class ClassUtil {
+    public static void covertFatherToChild(Object source, Object target) {
+        if ((target.getClass().getSuperclass() == source.getClass())) {
+            Field[] fieldList = source.getClass().getDeclaredFields();
+            for (Field field : fieldList) {
+                try {
+                    String fname = field.getName();
+                    fname = fname.substring(0, 1).toUpperCase() + fname.substring(1);
+                    Method getMethod = source.getClass().getMethod("get" + fname, null);
+                    Method setMethod = target.getClass().getMethod("set" + fname, field.getType());
+                    setMethod.invoke(target, getMethod.invoke(source, null));
+                    System.out.println(getMethod.invoke(source, null));
+                } catch (Exception e) {
+                    continue;
+                }
+            }
+        }
+    }
+
+}
+
